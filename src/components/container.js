9e1832e62c2cf8d21dd1765074e32746864636d5
Gilead Netto <gileadnetto@hotmail.com>
2023-06-28 18:09:20 -0300

    ajuste Rodapé

--- src/components/container.js
+++ src/components/container.js
@@ -1,15 +1,7 @@
 
 
 export default function Container(props) {
-
-    
-
-
-    {/* width: 100vw;
-                            max-width: 1000px;
-                            height: 1000px;
-                            max-height: calc(100vh - 376px); */}
-
+   
     return (
         <div  className={props?.main ? 'container-site container-main' : '' + ' container-site'}>
             <div className={props?.main ? 'bg-cor-fundo' : ''}>
