86128413c52d59d2741a621f53dc6e060e5493a6
Gilead Netto <gileadnetto@hotrmail.com>
2023-07-02 21:50:10 -0300

    teste cache

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -34,7 +34,10 @@ export default function Menu(props:ProdProps) {
             const protocol = window.location.protocol;
             const host = window.location.host;
             const url = protocol + '//' + host + '/api';
-            let response = await fetch(url);
+            let response = await fetch(url,{
+                cache: 'force-cache', 
+                next: { revalidate:60 }
+              });
             let res = await response.json();
             res = JSON.parse(res);
             setDados(res);
